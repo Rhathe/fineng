eb551cdf4941bf6e78394ad89b833aa77cbd1e95
Ramon Sandoval <rhathe@wrath.com>
2012-08-17 22:03:33 -0400

    Data stuff

--- finengproj/finengapp/models.py
+++ finengproj/finengapp/models.py
@@ -1,1 +1,18 @@
+from django.db import models
 
+class Stock(models.Model):
+	name = models.CharField(max_length=255)
+	ticker = models.CharField(max_length=255)
+	
+	def __repr__(self):
+		return self.name
+	
+class Price(models.Model):
+	value = models.DecimalField(max_digits=6, decimal_places=2)
+	date = models.DateTimeField(auto_now_add=True, db_index=True)
+	asset = models.ForeignKey("Stock")
+	class Meta:
+		get_latest_by = 'date'
+	
+	def __repr__(self):
+		return self.value
--- finengproj/finengapp/views.py
+++ finengproj/finengapp/views.py
@@ -1,20 +1,22 @@
 # Create your views here.
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.contrib.auth import authenticate, login
 from django.core.context_processors import csrf
 from django.template import RequestContext
 from django.contrib.auth import logout
 from django.http import HttpResponse
+from models import *
 
 def login_user(request):
     # get CSRF token
-    # c = {}
-    # c.update(csrf(request))
+    c = {}
+    c.update(csrf(request))
 
     state = "Please log in below..."
     returnsite = 'auth.html'
     username = password = ''
+    stockset = Stock.objects.all()
     
     if request.POST:
         username = request.POST.get('username')
@@ -31,11 +33,8 @@ def login_user(request):
         else:
             state = "Your username and/or password were incorrect."
 
-    return render_to_response(returnsite,{'state':state, 'username': username},context_instance=RequestContext(request))
+    return render_to_response(returnsite,{'state':state, 'username': username, 'stockset': stockset},context_instance=RequestContext(request))
 
 def logout_user(request):
 	logout(request)
-	state = "You have Logged Out"
-	returnsite = 'auth.html'
-	username = ''
-	return render_to_response(returnsite,{'state':state, 'username': username},context_instance=RequestContext(request))
+	return redirect('/')
